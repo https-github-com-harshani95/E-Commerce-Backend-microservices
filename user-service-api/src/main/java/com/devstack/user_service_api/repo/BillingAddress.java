835283baebd95d0e4617a6fbb365e17426d1e453
harshani <ruwanthikapemarathna@gmail.com>
2024-08-14 22:20:00 +0530

    created repo and dto with services

---
+++ user-service-api/src/main/java/com/devstack/user_service_api/repo/BillingAddress.java
@@ -0,0 +1,8 @@
+package com.devstack.user_service_api.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EnableJpaRepositories
+public interface BillingAddress extends JpaRepository<BillingAddress, String> {
+}
